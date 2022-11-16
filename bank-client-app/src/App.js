ac6f9974a39cf70ef1ed5a9f6ca6cd14df026715
Or <orlevy11@gmail.com>
2022-11-16 18:02:25 +0200

    made navBar

--- bank-client-app/src/App.js
+++ bank-client-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    const state = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <div className="header">
+            <ResponsiveAppBar />
+          </div>
+        </div>
+
+        {/* <Route
+            exact
+            path="/"
+            render={() => (
+              <Home
+                state={state}
+                logIn={this.logIn}
+                logOut={this.logOut}
+                addNewUser={this.addNewUser}
+              />
+            )}
+          /> */}
+      </Router>
+    );
+  }
 }
-
-export default App;
